192db6d88565a5904ec26b2342dd7b3d1f528f62
FvonAhn <fredd24hate@gmail.com>
2025-01-09 14:34:07 +0100

    Added the test to the IdGenerator. Everything works fine. Seems like everything on the G part is done. Thanks for playing.

---
+++ MyContactList_Test/Helpers/IdGenerator_Test.cs
@@ -0,0 +1,16 @@
+﻿using Business.Helpers;
+
+namespace MyContactList_Test.Helpers;
+public class IdGenerator_Test
+{
+    [Fact]
+    public void Generate_ShouldReturnGuidAsString()
+    {
+        // act
+        var result = IdGenerator.GenerateID();
+
+        // assert
+        Assert.NotNull(result);
+        Assert.True(Guid.TryParse(result, out _));
+    }
+}
