6f6833b5189002510073333dd75c7f87b1f22fdf
FvonAhn <fredd24hate@gmail.com>
2025-01-09 14:22:24 +0100

    Added tests to the ContactFactory.

---
+++ MyContactList_Test/Factories/ContactFactory_Test.cs
@@ -0,0 +1,50 @@
+﻿using Business.Factories;
+using Business.Models;
+
+namespace MyContactList_Test.Factories;
+public class ContactFactory_Test
+{
+    [Fact]
+    public void Create_ShouldReturnContactForm()
+    {
+        // Act
+        var result = ContactFactory.Create();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<ContactForm>(result);
+    }
+
+    [Fact]
+
+    public void Create_ShouldReturnContact_WhenContactFormIsProvided() 
+    {
+        // Arrange
+        var contactForm = new ContactForm()
+        {
+            FirstName = "Fred",
+            LastName = "Flinta",
+            Adress = "Flintgatan 1",
+            PostalCode = "12345",
+            City = "Flintville",
+            Email = "fred.flinta@flintstones.com",
+            Phone = "0123456789",
+        };
+
+        // Act
+        var result = ContactFactory.Create(contactForm);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Contact>(result);
+        Assert.Equal(contactForm.FirstName, result.FirstName);
+        Assert.Equal(contactForm.LastName, result.LastName);
+        Assert.Equal(contactForm.Adress, result.Adress);
+        Assert.Equal(contactForm.PostalCode, result.PostalCode);
+        Assert.Equal(contactForm.City, result.City);
+        Assert.Equal(contactForm.Email, result.Email);
+        Assert.Equal(contactForm.Phone, result.Phone);
+    }
+    
+
+}
